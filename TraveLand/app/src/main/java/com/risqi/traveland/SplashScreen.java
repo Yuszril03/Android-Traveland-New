e56d5e6e8b9f6cc2d6b94e8044dde781ee37c311
Muhammadiki <34560594+Muhammadiki@users.noreply.github.com>
2022-07-20 12:07:42 +0800

    up

--- TraveLand/app/src/main/java/com/risqi/traveland/SplashScreen.java
+++ TraveLand/app/src/main/java/com/risqi/traveland/SplashScreen.java
@@ -23,7 +23,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, LoginScreen.class);
+                Intent intent = new Intent(SplashScreen.this, MainMenu.class);
                 startActivity(intent);
                 Animatoo.animateFade(SplashScreen.this);
                 finish();
